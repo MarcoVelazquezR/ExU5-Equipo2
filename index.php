7079f4309ab27b16329740ae126a76177d5c69e4
MarcoVelazquezR <marcovelazquez124@hotmail.com>
2023-11-17 19:05:19 -0600

    Inicializacion de Repositorio

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+include 'includes/Edgar.php';
+?>
+//Caro
+<?php
+include 'includes/Caro.php';
+?>
+//Alex
+<?php
+include 'includes/Alex.php';
+?>
+//David
+<?php
+include 'includes/David.php';
+?>
+//Marco
+<?php
+include 'includes/Marco.php';
+?>
