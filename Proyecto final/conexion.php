9a4761d4867466238b00dc68b5d3cfc5c9d87493
Davidgf27 <lc18280501@toluca.tecnm.mx>
2023-11-20 13:01:15 -0600

    Primer commit

---
+++ Proyecto final/conexion.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$dbname = 'veterinaria';
+$user = 'davidgf';
+$password = '123456';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error al conectar a la base de datos: " . $e->getMessage());
+}
