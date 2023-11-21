ea8882f0307fc49e7376f2cdbe71c98d750db4b3
carolinaGE <cgarciae1@toluca.tecnm.mx>
2023-11-21 01:44:36 -0600

    Commit

--- veterinarios.php
+++ veterinarios.php
@@ -3,20 +3,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Veterinarios</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="/Proyecto final/CSS/styles.css">
     <style>
+        
         .table-striped thead tr th {
             background-color: #444; 
-            color: #fff; 
+            color: 
+            #fff; 
         }
     </style>
 </head>
 <body>
+    <header width="100%">
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+                <a href="#" class="navbar-brand">
+                    <img width="40px" height="40px" src="/Proyecto final/img/ico.ico" alt="mi img">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="menu">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a href="#" id="login-link" class="nav-link text-white">Bienvenida Admin, Carolina García Echegaray</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+    </header>
+
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div class="container-fluid">
+            <h1 class="h1 text-center text-white">VETERINARIOS</h1>
+        </div>
+    </nav>
+    
     <?php
     require_once('ORM/Database.php');
     require_once('ORM/orm.php');
@@ -40,7 +70,7 @@
     }
 
     ?>
-
+    
     <div class="table-responsive">
         <table class="table table-bordered table-striped">
             <thead>
@@ -68,16 +98,24 @@
 
                         <td class="Eliminar">
                             <p data-placement="top" data-toggle="tooltip" title="Delete">
-                                <a href="delete_veterinario.php?id=<?= $veterinario['id']; ?>" class="btn btn-danger btn-xs">
+                                <a href="javascript:void(0);" onclick="confirmarEliminar(<?= $veterinario['id']; ?>)" class="btn btn-danger btn-xs">
                                     <span class="glyphicon glyphicon-trash"></span>
                                 </a>
                             </p>
                         </td>
+
 
                     </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
     </div>
+    <script>
+        function confirmarEliminar(id) {
+            if (confirm('¿Estás seguro que deseas eliminar este veterinario?')) {
+                window.location.href = 'delete_veterinario.php?id=' + id;
+            }
+        }
+    </script>
 </body>
 </html>
